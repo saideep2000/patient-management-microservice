2e16c9a20a76ec125b815879587906527e74b64f
saideepsamineni <saminenisaideepchowdary@gmail.com>
2025-05-23 09:18:41 -0400

    Done with the billing service and it's grpc

--- patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -1,5 +1,7 @@
 package com.pm.patientservice.dto;
 
+import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -17,10 +19,10 @@ public class PatientRequestDTO {
   @NotBlank(message = "Address is required")
   private String address;
 
-  @NotNull(message = "Date of birth is required")
+  @NotBlank(message = "Date of birth is required")
   private String dateOfBirth;
 
-  @NotNull(message = "Registered date is required")
+  @NotBlank(groups = CreatePatientValidationGroup.class, message = "Registered date is required")
   private String registeredDate;
 
   public String getName() {
